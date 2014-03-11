b3dc7fe5607651d373d16c30ec4eab174d6a2e57
David Moebius <dmoebius@gmail.com>
2014-03-11 14:35:19 +0100

    [task] initial import

---
+++ src/main/java/org/osiam/web/util/SimpleAccessToken.java
@@ -0,0 +1,64 @@
+package org.osiam.web.util;
+
+import org.osiam.client.oauth.AccessToken;
+
+public class SimpleAccessToken extends AccessToken {
+
+    public SimpleAccessToken(String token) {
+        this.token = token;
+    }
+    
+    /**
+     * Retrieve the string value of the access token used to authenticate against the provider.
+     * @return The access token string
+     */
+    @Override
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * use the basic class {@link AccessToken} to retrieve type
+     * @exception UnsupportedOperationException
+     */
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * use the basic class {@link AccessToken} to retrieve experiseIn
+     * @exception UnsupportedOperationException
+     */
+    @Override
+    public int getExpiresIn() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * use the basic class {@link AccessToken} to retrieve expired
+     * @exception UnsupportedOperationException
+     */
+    @Override
+    public boolean isExpired() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * use the basic class {@link AccessToken} to retrieve scope
+     * @exception UnsupportedOperationException
+     */
+    @Override
+    public String getScope() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * use the basic class {@link AccessToken} to retrieve refreshToken
+     * @exception UnsupportedOperationException
+     */
+    @Override
+    public String getRefreshToken() {
+        throw new UnsupportedOperationException();
+    }
+}
