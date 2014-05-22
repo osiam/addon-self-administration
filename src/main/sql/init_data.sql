a368d658f8b6b4673e18c9469918256c7fff1082
David Crome <d.crome@tarent.de>
2014-05-22 18:27:53 +0200

    [fix] fixes and cleanup for release 1.0

--- src/main/sql/init_data.sql
+++ src/main/sql/init_data.sql
@@ -1,13 +1,9 @@
 --
--- Updating hibernate sequence start value
---
-SELECT pg_catalog.setval('hibernate_sequence', 10, false);
-
---
--- Extension data for registration purpose
+-- Extension data, according to the attribute values in the addon-self-administration.properties
+-- Have to be imported in the database of the resource server, before you deploy the addon-self-administration!
 --
 
-INSERT INTO scim_extension VALUES (5, 'urn:scim:schemas:osiam:2.0:Registration');
+INSERT INTO scim_extension VALUES (5, 'urn:org.osiam:scim:extensions:addon-self-administration');
 
 INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
 	VALUES (6, false, 'activationToken', 'STRING', 5);
