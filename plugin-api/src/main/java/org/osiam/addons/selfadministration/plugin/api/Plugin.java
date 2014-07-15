f735fcc71e8e93d701b433eee4e8afa5101ad10c
Timo Kanera <t.kanera@tarent.de>
2014-07-15 13:02:25 +0200

    added plug-in method for post registration actions

--- plugin-api/src/main/java/org/osiam/addons/selfadministration/plugin/api/Plugin.java
+++ plugin-api/src/main/java/org/osiam/addons/selfadministration/plugin/api/Plugin.java
@@ -7,10 +7,18 @@ import org.osiam.resources.scim.User;
  */
 public interface Plugin {
 
-	/**
-	 * Performs the plugin action.
-	 */
-	public void performPreRegistrationCheck(User user) throws RegistrationFailedException;
+    /**
+     * Performs the plugin action.
+     */
+    public void performPreRegistrationCheck(User user) throws RegistrationFailedException;
 
+    /**
+     * Performs post registration actions for the given {@link User}.
+     * 
+     * @param user
+     *        the newly registered user
+     * @throws PostRegistrationFailedException
+     *         if the post registration steps failed
+     */
+    public void performPostRegistrationActions(User user) throws PostRegistrationFailedException;
 }
-
