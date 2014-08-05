6d8d2a3a675fae7de598bf7ff81e75994dcf83ce
Max Marche <m.marche@tarent.de>
2014-08-05 10:55:15 +0200

    Add @Controller annotation

--- self-administration/src/main/java/org/osiam/addons/selfadministration/controller/AccountDeletionController.java
+++ self-administration/src/main/java/org/osiam/addons/selfadministration/controller/AccountDeletionController.java
@@ -32,6 +32,7 @@ import org.osiam.client.oauth.AccessToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * A controller providing an operation for account deletion.
  */
+@Controller
 public class AccountDeletionController {
 
     @Inject
