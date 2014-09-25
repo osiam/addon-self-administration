56060db56379a814c210ecdd3ff0e3071c971565
Thomas Krille <tkrille@gmail.com>
2014-09-26 00:19:24 +0200

    [fix] plugin system fails on missing properties
    when setting 'plugin.enabled' to true in 'addon-self-administration.properties'
    but not providing values for 'plugin.jar.path' or 'plugin.classname' an
    exception was thrown at runtime. now such misconfiguration will be
    detected at deployment time, failing the deployment process early. due
    to this changes the plugin will be loaded after bean initialization
    instead of when the first call is made to the plugin handler.

---
+++ src/main/java/org/osiam/addons/self_administration/util/NoopPlugin.java
@@ -0,0 +1,17 @@
+package org.osiam.addons.self_administration.util;
+
+import org.osiam.addons.self_administration.plugin_api.CallbackException;
+import org.osiam.addons.self_administration.plugin_api.CallbackPlugin;
+import org.osiam.resources.scim.User;
+
+public class NoopPlugin implements CallbackPlugin {
+    @Override
+    public void performPreRegistrationActions(final User user) throws CallbackException {
+        // no-op
+    }
+
+    @Override
+    public void performPostRegistrationActions(final User user) throws CallbackException {
+        // no-op
+    }
+}
