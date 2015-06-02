55a00f6487e4fb32cdfd0f109f8ffe63beeebfbc
Thomas Krille <tkrille@gmail.com>
2015-06-02 18:12:08 +0200

    [cleanup] solve code violations from Sonar

--- src/main/java/org/osiam/addons/self_administration/registration/RegistrationController.java
+++ src/main/java/org/osiam/addons/self_administration/registration/RegistrationController.java
@@ -33,7 +33,6 @@ import org.osiam.resources.scim.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -42,7 +41,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping(value = "/registration")
